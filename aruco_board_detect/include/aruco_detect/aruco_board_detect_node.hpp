47599be91411239406de1c1ca4b0c9e2feccb5e6
fbottarel <fabrizio.bottarel@iit.it>
2020-11-05 18:09:47 +0000

    added ImageConverter class and other stubs

--- aruco_board_detect/include/aruco_detect/aruco_board_detect_node.hpp
+++ aruco_board_detect/include/aruco_detect/aruco_board_detect_node.hpp
@@ -8,11 +8,57 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
 
 // OpenCV + ArUCO
 #include <opencv2/opencv.hpp>
 #include <opencv2/aruco.hpp>
 #include <opencv2/highgui/highgui.hpp>
+
+#include <mutex>
+
+
+class ImageConverter
+{
+    // this class is supposed to catch all camera images and store them locally
+
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber image_sub_;
+    image_transport::Publisher image_pub_;
+
+    cv_bridge::CvImagePtr current_img_ptr_;
+
+    std::mutex image_mutex_;
+
+    std::string debug_window_name_;
+
+public:
+
+    ImageConverter(ros::NodeHandle& nh);
+
+    ~ImageConverter();
+
+    void getCurrentImage(cv::Mat& cv_image);
+
+    void imageAcquisitionCallback(const sensor_msgs::ImageConstPtr& msg);
+
+};
+
+
+class ArucoBoardDetector
+{
+    // this class is fed images and computes the pose of the board once in a while
+
+};
+
+class ArucoDetectNode
+{
+    // this class has an image converter and a board detector inside, sets up callbacks and calls the board detector once in a while
+    // also publishes a tf pose with the board pose
+
+
+};
+
 
 namespace aruco_board_detect
 {
--- aruco_board_detect/src/aruco_detect/aruco_board_detect_node.cpp
+++ aruco_board_detect/src/aruco_detect/aruco_board_detect_node.cpp
@@ -19,19 +19,84 @@ void aruco_board_detect::imageCallback(const sensor_msgs::ImageConstPtr& msg)
 
 }
 
-
-int main(int argc, char** argv )
+ImageConverter::ImageConverter(ros::NodeHandle& nh) : it_(nh)
 {
-    ros::init(argc, argv, "image_listener_tutorial");
+    // Subscribe to the camera image topic
+
+    image_sub_ = it_.subscribe("/camera/color/image_raw", 1, &ImageConverter::imageAcquisitionCallback, this);
+
+    debug_window_name_ = "Input camera image";
+    cv::namedWindow(debug_window_name_);
+
+}
+
+ImageConverter::~ImageConverter()
+{
+    cv::destroyWindow(debug_window_name_);
+}
+
+void ImageConverter::getCurrentImage(cv::Mat& cv_image)
+{
+    // Assign the image field of the current cv_bridge::cvImage
+
+    image_mutex_.lock();
+
+    cv_image = current_img_ptr_->image;
+
+    image_mutex_.unlock();
+
+    return;
+}
+
+void ImageConverter::imageAcquisitionCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+    // Copy the image from the sensor_msg and store it internally
+
+    image_mutex_.lock();
+
+    try
+    {
+        current_img_ptr_ = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+
+        cv::imshow(debug_window_name_, current_img_ptr_->image);
+        cv::waitKey(3);
+    }
+    catch (cv_bridge::Exception& exc)
+    {
+        ROS_ERROR("Caught cv_bridge exception: %s", exc.what());
+    }
+
+    image_mutex_.unlock();
+
+    return;
+
+}
+
+// int main(int argc, char** argv )
+// {
+//     ros::init(argc, argv, "image_listener_tutorial");
+//     ros::NodeHandle nh;
+
+//     cv::namedWindow("view");
+//     cv::startWindowThread();
+
+//     image_transport::ImageTransport it(nh);
+//     image_transport::Subscriber sub = it.subscribe("camera/color/image_raw", 1, aruco_board_detect::imageCallback);
+
+//     ros::spin();
+//     cv::destroyWindow("view");
+
+// }
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "aruco_board_detect_node");
     ros::NodeHandle nh;
 
-    cv::namedWindow("view");
-    cv::startWindowThread();
+    ImageConverter image_converter(nh);
 
-    image_transport::ImageTransport it(nh);
-    image_transport::Subscriber sub = it.subscribe("camera/color/image_raw", 1, aruco_board_detect::imageCallback);
+    if (!ros::isShuttingDown)
+        ros::spinOnce();
 
-    ros::spin();
-    cv::destroyWindow("view");
-
+    return 0;
 }
