2bf52aefed939064a46569bc023e75f204d397b4
fbottarel <fabrizio.bottarel@iit.it>
2020-12-30 15:04:43 +0100

    added custom config for single markers

--- aruco_board_detect/src/aruco_detect/aruco_board_detect_node.cpp
+++ aruco_board_detect/src/aruco_detect/aruco_board_detect_node.cpp
@@ -175,11 +175,11 @@ ArucoDetectNode::ArucoDetectNode(ros::NodeHandle& nh) : nh_(nh), time_between_ca
 
     // Set up the board
 
-    nh.param<int>("number_markers_x", board_description_.n_markers_x_, 7);
-    nh.param<int>("number_markers_y", board_description_.n_markers_y_, 5);
-    nh.param<float>("marker_edge_size", board_description_.marker_size_, 0.057);
-    nh.param<float>("marker_stride", board_description_.marker_stride_, 0.02);
-    nh.param<int>("dictionary_type", board_description_.dict_type_, cv::aruco::DICT_4X4_50);
+    nh.param<int>("board_config/number_markers_x", board_description_.n_markers_x_, 7);
+    nh.param<int>("board_config/number_markers_y", board_description_.n_markers_y_, 5);
+    nh.param<float>("board_config/marker_edge_size", board_description_.marker_size_, 0.057);
+    nh.param<float>("board_config/marker_stride", board_description_.marker_stride_, 0.02);
+    nh.param<int>("board_config/dictionary_type", board_description_.dict_type_, cv::aruco::DICT_4X4_50);
 
     aruco_dict_ = cv::aruco::getPredefinedDictionary(board_description_.dict_type_);
     aruco_board_ = cv::aruco::GridBoard::create(board_description_.n_markers_x_,
@@ -190,9 +190,9 @@ ArucoDetectNode::ArucoDetectNode(ros::NodeHandle& nh) : nh_(nh), time_between_ca
 
     // Set up single marker data
 
-    nh.param<float>("marker_edge_size", single_markers_description_.marker_size_);
-    nh.param<int>("dictionary_type", single_markers_description_.dict_type_, cv::aruco::DICT_4X4_50);
-    nh.param<std::vector<int>>("marker_ids", single_markers_description_.marker_ids_, std::vector<int>());
+    nh.param<float>("single_markers_config/marker_edge_size", single_markers_description_.marker_size_);
+    nh.param<int>("single_markers_config/dictionary_type", single_markers_description_.dict_type_, cv::aruco::DICT_4X4_50);
+    nh.param<std::vector<int>>("single_markers_config/marker_ids", single_markers_description_.marker_ids_, std::vector<int>());
 
 }
 
@@ -254,10 +254,14 @@ void ArucoDetectNode::boardDetectionTimedCallback(const ros::TimerEvent&)
     std::vector<std::vector<cv::Point2f>> marker_corners;
     std::vector<cv::Vec3d> marker_rvecs, marker_tvecs;
 
+    cv::aruco::detectMarkers(input_img_, aruco_dict_, marker_corners, marker_ids);
+
+    // Split the vector of detected markers
+
     std::vector<int> single_marker_ids;
     std::vector<std::vector<cv::Point2f>> single_marker_corners;
-
-    cv::aruco::detectMarkers(input_img_, aruco_dict_, marker_corners, marker_ids);
+    std::vector<int> board_marker_ids;
+    std::vector<std::vector<cv::Point2f>> board_marker_corners;
 
     // Using Vec3d causes the estimatePoseBoard method to use them as initial guess in opencv3.2
     // resulting in random estimation
@@ -269,18 +273,42 @@ void ArucoDetectNode::boardDetectionTimedCallback(const ros::TimerEvent&)
     // Compute board pose if at least one marker is detected
     if (marker_ids.size() > 0)
     {
+        // By knowing the number of markers in the board, we can filter out from the estimation the ones whose
+        // ID is not within bounds
+        // e.g. a 7x5 board has markers with ids ranging from 1 to 35
+        // Split the markers that belong to the board and the singles
+
+        for (int detected_marker_idx = 0; detected_marker_idx < marker_ids.size(); detected_marker_idx++)
+        {
+            if (marker_ids[detected_marker_idx] > 0 && marker_ids[detected_marker_idx] < board_description_.n_markers_x_ * board_description_.n_markers_y_)
+            {
+                // Marker belongs to board
+
+                board_marker_ids.push_back(marker_ids[detected_marker_idx]);
+                board_marker_corners.push_back(marker_corners[detected_marker_idx]);
+            }
+
+            if (detect_single_markers_ && single_markers_description_.marker_ids_.size())
+            {
+                if (std::find(single_markers_description_.marker_ids_.begin(),
+                              single_markers_description_.marker_ids_.end(),
+                              marker_ids[detected_marker_idx]) != single_markers_description_.marker_ids_.end())
+                {
+                    // Marker is a single desired marker
+
+                    single_marker_ids.push_back(marker_ids[detected_marker_idx]);
+                    single_marker_corners.push_back(marker_corners[detected_marker_idx]);
+                }
+            }
+        }
+
         // Draw all the markers for maximum debugging
         input_img_.copyTo(output_img_);
-<<<<<<< Updated upstream
-=======
-
-        cv::aruco::drawDetectedMarkers(output_img_, marker_corners, marker_ids);
->>>>>>> Stashed changes
 
         // Attempt to estimate board pose, and if successful draw origin and axes
         // rotation and position are both assumed to be arrays of 3 numbers
 
-        int board_estimation_outcome = cv::aruco::estimatePoseBoard(marker_corners, marker_ids, aruco_board_,
+        int board_estimation_outcome = cv::aruco::estimatePoseBoard(board_marker_corners, board_marker_ids, aruco_board_,
                                          cam_params_->getCameraMatrix(),
                                          cam_params_->getDistortionCoeffs(),
                                          board_rotation, board_position
@@ -289,6 +317,8 @@ void ArucoDetectNode::boardDetectionTimedCallback(const ros::TimerEvent&)
         {
             // std::cout << "Board_position: " << std::endl << board_position << std::endl;
             // std::cout << "Board_rotation: " << std::endl << board_rotation << std::endl;
+
+            ROS_INFO_STREAM("Marker board detected");
 
             // Obtain a proper rotation matrix from rotation vector
 
@@ -346,7 +376,12 @@ void ArucoDetectNode::boardDetectionTimedCallback(const ros::TimerEvent&)
                                                                 "graspa_board");
             board_transform_bc_.sendTransform(board_graspa_stamped_transform);
 
-            // Draw the board ref system
+            // Draw the board ref system and markers
+
+            cv::aruco::drawDetectedMarkers(output_img_,
+                                          board_marker_corners,
+                                          board_marker_ids,
+                                          cv::Scalar(0, 255, 0));
 
             cv::aruco::drawAxis(output_img_,
                                 cam_params_->getCameraMatrix(),
@@ -362,73 +397,75 @@ void ArucoDetectNode::boardDetectionTimedCallback(const ros::TimerEvent&)
 
             ROS_INFO_STREAM("Detecting single markers with " << marker_corners.size() << "corners.");
 
-            cv::aruco::estimatePoseSingleMarkers(marker_corners, board_description_.marker_size_,
-                                    cam_params_->getCameraMatrix(),
-                                    cam_params_->getDistortionCoeffs(),
-                                    markers_rot, markers_pos);
-
-            ROS_INFO_STREAM("Single markers detected");
-
-            // Prepare the marker list array message including every detected marker
-
-            aruco_board_detect::MarkerListPtr marker_list_msg(new aruco_board_detect::MarkerList);
-
-            marker_list_msg->header.frame_id = cam_params_->getCameraFrameId();
-            marker_list_msg->header.stamp = ros::Time::now();
-            marker_list_msg->marker_dictionary.data = board_description_.dict_type_;
-
-            for (int idx=0; idx < markers_rot.size(); idx++)
+            if (single_marker_ids.size())
             {
-                // Rotation axis to rotation matrix
+                ROS_INFO_STREAM("Single markers detected");
 
-                cv::Mat rot_mat_marker(3, 3, cv::DataType<float>::type);
-                cv::Rodrigues(markers_rot[idx], rot_mat_marker);
+                cv::aruco::estimatePoseSingleMarkers(single_marker_corners, single_markers_description_.marker_size_,
+                                        cam_params_->getCameraMatrix(),
+                                        cam_params_->getDistortionCoeffs(),
+                                        markers_rot, markers_pos);
 
-                // Rotation matrix to quaternion
+                // Prepare the marker list array message including every detected marker
 
-                tf::Matrix3x3 rot_mat_marker_tf(rot_mat_marker.at<double>(0,0), rot_mat_marker.at<double>(0,1), rot_mat_marker.at<double>(0,2),
-                                    rot_mat_marker.at<double>(1,0), rot_mat_marker.at<double>(1,1), rot_mat_marker.at<double>(1,2),
-                                    rot_mat_marker.at<double>(2,0), rot_mat_marker.at<double>(2,1), rot_mat_marker.at<double>(2,2));
+                aruco_board_detect::MarkerListPtr marker_list_msg(new aruco_board_detect::MarkerList);
 
-                tf::Quaternion quat_marker_tf;
-                rot_mat_marker_tf.getRotation(quat_marker_tf);
+                marker_list_msg->header.frame_id = cam_params_->getCameraFrameId();
+                marker_list_msg->header.stamp = ros::Time::now();
+                marker_list_msg->marker_dictionary.data = board_description_.dict_type_;
 
-                // Push back pose and index
+                for (int idx=0; idx < markers_rot.size(); idx++)
+                {
+                    // Rotation axis to rotation matrix
 
-                geometry_msgs::Pose marker_pose;
-                marker_pose.orientation.x = quat_marker_tf.getX();
-                marker_pose.orientation.y = quat_marker_tf.getY();
-                marker_pose.orientation.z = quat_marker_tf.getZ();
-                marker_pose.orientation.w = quat_marker_tf.getW();
-                marker_pose.position.x = markers_pos[idx][0];
-                marker_pose.position.y = markers_pos[idx][1];
-                marker_pose.position.z = markers_pos[idx][2];
-                marker_list_msg->marker_poses.push_back(marker_pose);
+                    cv::Mat rot_mat_marker(3, 3, cv::DataType<float>::type);
+                    cv::Rodrigues(markers_rot[idx], rot_mat_marker);
 
-                std_msgs::Int16 marker_id;
-                marker_id.data = marker_ids[idx];
-                marker_list_msg->marker_ids.push_back(marker_id);
+                    // Rotation matrix to quaternion
 
-                // Draw the single marker axes
+                    tf::Matrix3x3 rot_mat_marker_tf(rot_mat_marker.at<double>(0,0), rot_mat_marker.at<double>(0,1), rot_mat_marker.at<double>(0,2),
+                                        rot_mat_marker.at<double>(1,0), rot_mat_marker.at<double>(1,1), rot_mat_marker.at<double>(1,2),
+                                        rot_mat_marker.at<double>(2,0), rot_mat_marker.at<double>(2,1), rot_mat_marker.at<double>(2,2));
 
-                cv::aruco::drawAxis(output_img_,
-                                    cam_params_->getCameraMatrix(),
-                                    cam_params_->getDistortionCoeffs(),
-                                    markers_rot[idx],
-                                    markers_pos[idx],
-                                    0.1);
+                    tf::Quaternion quat_marker_tf;
+                    rot_mat_marker_tf.getRotation(quat_marker_tf);
 
+                    // Push back pose and index
+
+                    geometry_msgs::Pose marker_pose;
+                    marker_pose.orientation.x = quat_marker_tf.getX();
+                    marker_pose.orientation.y = quat_marker_tf.getY();
+                    marker_pose.orientation.z = quat_marker_tf.getZ();
+                    marker_pose.orientation.w = quat_marker_tf.getW();
+                    marker_pose.position.x = markers_pos[idx][0];
+                    marker_pose.position.y = markers_pos[idx][1];
+                    marker_pose.position.z = markers_pos[idx][2];
+                    marker_list_msg->marker_poses.push_back(marker_pose);
+
+                    std_msgs::Int16 marker_id;
+                    marker_id.data = marker_ids[idx];
+                    marker_list_msg->marker_ids.push_back(marker_id);
+
+                    // Draw the single markers and axes
+
+                    cv::aruco::drawAxis(output_img_,
+                                        cam_params_->getCameraMatrix(),
+                                        cam_params_->getDistortionCoeffs(),
+                                        markers_rot[idx],
+                                        markers_pos[idx],
+                                        0.1);
+
+                }
+
+                cv::aruco::drawDetectedMarkers(output_img_,
+                                    single_marker_corners,
+                                    single_marker_ids,
+                                    cv::Scalar(0,0,255));
+
+                // Publish marker data
+
+                markers_data_pub_.publish(marker_list_msg);
             }
-
-            // Draw the marker corners and ids
-
-            cv::aruco::drawDetectedMarkers(output_img_,
-                    marker_corners,
-                    marker_ids);
-
-            // Publish marker data
-
-            markers_data_pub_.publish(marker_list_msg);
         }
 
         // Send the output image
